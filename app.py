c86510f47211484ea69c85556c19ee9e9aa99678
Fayez Nazzal <soulldev@gmail.com>
2022-06-15 11:27:27 +0300

    feat: :sparkles: serve vite app from flask

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 from routes.item_bp import item_bp
 
 print("Database created........")
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='client/dist')
 app.config.from_object('config')
 
 app.register_blueprint(item_bp, url_prefix='/item')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.debug = True
